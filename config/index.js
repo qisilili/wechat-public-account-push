1c3b82a90c557a10369a2d503c106d7dc3eaaa4b
qisilili <113961848+qisilili@users.noreply.github.com>
2022-09-20 19:34:11 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,9 +35,6 @@ export const config = {
     // 古诗古文
     poetry: false,
 
-    /** 星座运势 */
-    horoscope: false,
-
     /** 生日消息和节日消息 */
     birthdayMessage: true,
   },
@@ -52,13 +49,9 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "bwgrwHu8W4dI6YeJA2tSNJujLL_e-aGl6nJhv1ETyOQ",
       // 所在省份
-      province: "四川",
+      PROVINCE: "四川",
       // 所在城市
-      city: "成都",
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
+      CITY: "成都",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
@@ -78,23 +71,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
-
-  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-  CALLBACK_USERS: [
-    {
-      // 一般都填自己
-      name: "自己",
-      // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
-    },
-    // 你可以不断按格式往下增加
-    // ...
-  ],
-
-  /**
-   * 信息配置
-   */
+  CALLBACK_TEMPLATE_ID: "bwgrwHu8W4dI6YeJA2tSNJujLL_e-aGl6nJhv1ETyOQ",
   SLOT_LIST: [
     // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
